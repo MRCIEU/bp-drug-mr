4419688d216b4ab4b19974c2a0acbce9aebc451c
AmandaHWChong <70328172+AmandaHWChong@users.noreply.github.com>
2023-07-05 18:04:57 +0100

    Create data_adj_by_med_status.r

---
+++ analysis/gwas/ldlc_adj_med_analysis/script/data_adj_by_med_status.r
@@ -0,0 +1,4 @@
+# Script outline
+# This script will create multiple LDL cholesterol columns and adjust each column based on multiple relative medication adjustments and multiple absolute medication adjustments
+
+
