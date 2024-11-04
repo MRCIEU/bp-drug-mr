6bdcccf5c4fd667c45c81b34ed98128a5fe70ab4
Gibran Hemani <explodecomputer@gmail.com>
2024-11-04 08:12:40 -0700

    batool method looks well implemented

--- analysis/mr_simulation/sim_pc_based_cis_mvmr_batool.R
+++ analysis/mr_simulation/sim_pc_based_cis_mvmr_batool.R
@@ -50,6 +50,7 @@ sseurx2 <- paramseurx2 %>% generate_gwas_ss(nid=100000, ldobj=eurld)
 sseurx3 <- paramseurx3 %>% generate_gwas_ss(nid=100000, ldobj=eurld)
 sseury <- paramseury %>% generate_gwas_ss(nid=100000, ldobj=eurld)
 
+save(sseurx1, sseurx2, sseurx3, sseury, eurld, file="sim_pc_based_cis_mvmr_batool.rdata")
 
 ## Perform analysis using PCs using Batool et al. 2022 approach
 
@@ -71,32 +72,64 @@ sseury <- paramseury %>% generate_gwas_ss(nid=100000, ldobj=eurld)
 # m <- flip %*% t(flip)
 # ld <- ld * m
 
+
+load("sim_pc_based_cis_mvmr_batool.rdata")
+
 # MV-PCA method (Batool et al. 2022)
 Psi = ((abs(sseurx1$bhat)+abs(sseurx2$bhat)+abs(sseurx3$bhat))/sseury$se)%o%
 ((abs(sseurx1$bhat)+abs(sseurx2$bhat)+abs(sseurx3$bhat))/sseury$se)*eurld$ld
 
-K = which(cumsum(prcomp(Psi, scale=FALSE)$sdev^2/
-sum((prcomp(Psi, scale=FALSE)$sdev^2)))>0.99)[1]
+# check using the kronecker product gives the same answer as element wise multiplication
+# Psi2 <- eurld$ld
+# nsnps = nrow(eurld$ld)
+# for(i in 1:nsnps){
+#     for(j in 1:nsnps){
+#          Psi2[i,j]  <- (abs(sseurx1$bhat[i]) + abs(sseurx2$bhat[i]) + abs(sseurx3$bhat[i])) * (abs(sseurx1$bhat[j]) + abs(sseurx2$bhat[j]) + abs(sseurx3$bhat[j])) / sseury$se[i] / sseury$se[j] * eurld$ld[i,j]
+#     }
+# }
+# cor(c(Psi), c(Psi2))
+# lm(c(Psi) ~ c(Psi2))
+
+pcs <- eigen(Psi)
+
+K <- which(cumsum(pcs$values^2) / sum(pcs$values^2) > 0.99)[1]
 K
 
-eurx1comp = as.numeric(sseurx1$bhat%*%prcomp(Psi, scale=FALSE)$rotation[,1:K])
-eurx2comp = as.numeric(sseurx2$bhat%*%prcomp(Psi, scale=FALSE)$rotation[,1:K])
-eurx3comp = as.numeric(sseurx3$bhat%*%prcomp(Psi, scale=FALSE)$rotation[,1:K])
-eurycomp = as.numeric(sseury$bhat%*%prcomp(Psi, scale=FALSE)$rotation[,1:K])
-eurycomp_se = as.numeric(sseury$se%*%prcomp(Psi, scale=FALSE)$rotation[,1:K])
+
+
+eurx1comp <- sseurx1$bhat %*% pcs$vectors[,1:K] %>% drop()
+eurx2comp <- sseurx2$bhat %*% pcs$vectors[,1:K] %>% drop()
+eurx3comp <- sseurx3$bhat %*% pcs$vectors[,1:K] %>% drop()
+eurycomp <- sseury$bhat %*% pcs$vectors[,1:K] %>% drop()
+eurycomp_se <- sseury$se %*% pcs$vectors[,1:K] %>% drop()
+
+
+
 
 # To do weights
 Omega <- sseury$se %o% sseury$se * eurld$ld
-pcOmega <- t(prcomp(Psi, scale=FALSE)$rotation[,1:K])%*%Omega%*%
-prcomp(Psi, scale=FALSE)$rotation[,1:K]
+pcOmega <- t(pcs$vectors[,1:K]) %*% Omega %*% pcs$vectors[,1:K]
+
+
 
 # Univariable MR (weighted)
 summary(lm(eurycomp ~ 0 + eurx1comp, weight = 1/diag(pcOmega)^2))
+summary(lm(eurycomp ~ 0 + eurx2comp, weight = 1/diag(pcOmega)^2))
+summary(lm(eurycomp ~ 0 + eurx3comp, weight = 1/diag(pcOmega)^2))
+
 
 # MV-PCA (weighted)
-mvpca = mr_mvivw(mr_mvinput(cbind(eurx1comp, eurx2comp, eurx3comp),
-cbind(rep(1, length(eurx1comp)), rep(1, length(eurx1comp)), rep(1, length(eurx1comp))),
-eurycomp, rep(1, length(eurx1comp)), corr=pcOmega), model="fixed")
+
+mvin <- mr_mvinput(
+    cbind(eurx1comp, eurx2comp, eurx3comp),
+    cbind(rep(1, length(eurx1comp)), rep(1, length(eurx1comp)), rep(1, length(eurx1comp))),
+    eurycomp, 
+    rep(1, length(eurx1comp)), 
+    corr=pcOmega
+)
+
+
+mvpca = mr_mvivw(mvin, model="fixed")
 mvpca$Estimate; mvpca$StdError
 
 mvpca_est = solve(rbind(eurx1comp, eurx2comp, eurx3comp)%*%solve(pcOmega)%*%
@@ -108,3 +141,39 @@ cbind(eurx1comp, eurx2comp, eurx3comp))))
 z_scores <- mvpca_est / mvpca_se
 p_values <- 2 * pnorm(abs(z_scores), lower.tail = FALSE)
 p_values
+
+
+
+## Try using unweighted LD matrix
+
+pcs2 <- eigen(eurld$ld)
+K2 <- which(cumsum(pcs2$values^2) / sum(pcs2$values^2) > 0.99)[1]
+K2
+eurx1comp2 <- sseurx1$bhat %*% pcs2$vectors[,1:K] %>% drop()
+eurx2comp2 <- sseurx2$bhat %*% pcs2$vectors[,1:K] %>% drop()
+eurx3comp2 <- sseurx3$bhat %*% pcs2$vectors[,1:K] %>% drop()
+eurycomp2 <- sseury$bhat %*% pcs2$vectors[,1:K] %>% drop()
+eurycomp_se2 <- sseury$se %*% pcs2$vectors[,1:K] %>% drop()
+
+cor(eurx1comp, eurx1comp2)
+
+cor(pcs$vectors[,1:K], pcs2$vectors[,1:K]) %>% diag
+pcOmega2 <- t(pcs2$vectors[,1:K]) %*% Omega %*% pcs2$vectors[,1:K]
+summary(lm(eurycomp2 ~ 0 + eurx1comp2, weight = 1/diag(pcOmega2)^2))
+summary(lm(eurycomp2 ~ 0 + eurx2comp2, weight = 1/diag(pcOmega2)^2))
+summary(lm(eurycomp2 ~ 0 + eurx3comp2, weight = 1/diag(pcOmega2)^2))
+
+
+mvin2 <- mr_mvinput(
+    cbind(eurx1comp2, eurx2comp2, eurx3comp2),
+    cbind(rep(1, length(eurx1comp2)), rep(1, length(eurx1comp2)), rep(1, length(eurx1comp2))),
+    eurycomp2, 
+    rep(1, length(eurx1comp2)), 
+    corr=pcOmega2
+)
+
+
+mvpca2 = mr_mvivw(mvin2, model="fixed")
+mvpca2$Estimate; mvpca2$StdError
+
+
